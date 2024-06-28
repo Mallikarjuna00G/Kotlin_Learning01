a909deab95b50d236ab5d086df3345fb554bea44
Mallikarjuna00G <mallikarjuna00g@gmail.com>
2024-06-28 19:09:43 +0530

    Project003: ByteShortIntLong

---
+++ ByteShortInt/src/Main.kt
@@ -0,0 +1,23 @@
+fun main() {
+    var number: Int = 30
+
+    val maxIntegerValue: Int = Int.MAX_VALUE
+    val minIntegerValue: Int = Int.MIN_VALUE
+    println("Int-Max: $maxIntegerValue")
+    println("Int-Min: $minIntegerValue")
+
+    val maxByteValue: Byte = Byte.MAX_VALUE
+    val minByteValue: Byte = Byte.MIN_VALUE
+    println("Byte-Max: $maxByteValue")
+    println("Byte-Min: $minByteValue")
+
+    val maxShortValue: Short = Short.MAX_VALUE
+    val minShortValue: Short = Short.MIN_VALUE
+    println("Short-Max: $maxShortValue")
+    println("Short-Min: $minShortValue")
+
+    val maxLongValue: Long = Long.MAX_VALUE
+    val minLongValue: Long = Long.MIN_VALUE
+    println("Long-Max: $maxLongValue")
+    println("Long-Min: $minLongValue")
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Kotlin_Learning01
 Learning kotlin from [Learn Kotlin Programming – Full Course for Beginners : freeCodeCamp](https://youtu.be/EExSSotojVI?si=4tJZHmVcVu1dXLC4)
+
+- Project001: [HelloWorld](./HelloWorld)
+- Project002: [Variables](./Variables)
+- Project003: [ByteShortInt](./ByteShortInt)
